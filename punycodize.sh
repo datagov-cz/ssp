f8c79511fcf7daf72895a148658541e8a50eb0a5
Petr Kremen <kremep1@fel.cvut.cz>
2018-09-03 13:13:35 +0200

    [UPD] disjointness axioms

--- punycodize.sh
+++ punycodize.sh
@@ -8,6 +8,7 @@ do
   for f in $FILES
   do
     echo $f
-    sed -i 's/slovník.gov.cz/xn--slovnk-7va.gov.cz/g' $f
+#   sed -i 's/slovník.gov.cz/xn--slovnk-7va.gov.cz/g' $f
+    sed -i 's/xn--slovnk-7va.gov.cz/slovník.gov.cz/g' $f
   done
 done
