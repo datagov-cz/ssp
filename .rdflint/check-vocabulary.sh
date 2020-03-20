3a110f7a19e7311eafab104d384d4e5d28fe9053
Petr Kremen <kremep1@fel.cvut.cz>
2020-03-20 10:24:48 +0100

    Only errors fail the pipeline

--- .rdflint/check-vocabulary.sh
+++ .rdflint/check-vocabulary.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 VOCABULARY=$1
 mkdir -p rdflint-output/$VOCABULARY
-java -jar .rdflint/rdflint-0.1.3.jar \
+java -jar .rdflint/rdflint-custom.jar \
   -config .rdflint/rdflint-config.yml \
   -targetdir $VOCABULARY \
   -outputdir \
+  -minErrorLevel ERROR \
   ./rdflint-output/$VOCABULARY
