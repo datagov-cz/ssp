084ed3808a76fc2df6b346531c1f03bcef0e4a66
Petr Křemen <petr.kremen@gmail.com>
2021-02-02 01:19:02 +0100

    Clearing whole SSP endpoint before upload. Pipeline optimization and refactoring

--- validation/src/test/java/cz/gov/ssp/ExportSGoV.java
+++ validation/src/test/java/cz/gov/ssp/ExportSGoV.java
@@ -3,31 +3,25 @@ package cz.gov.ssp;
 import static cz.gov.ssp.OntologyUtils.addTypeToModel;
 import static org.junit.jupiter.api.Assertions.fail;
 
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
-import org.apache.jena.vocabulary.RDF;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Export {
+public class ExportSGoV {
 
-    private static final Logger log = LoggerFactory.getLogger(Export.class);
-
-    private static final String gsp = "http://plugins.linkedpipes.com/ontology/x-graphStorePurger#";
+    private static final Logger log = LoggerFactory.getLogger(ExportSGoV.class);
 
     @Test
     public void export() throws IOException {
@@ -47,8 +41,6 @@ public class Export {
         RDFDataMgr.write(new FileOutputStream("../sgov.trig"), dataset, RDFFormat.TRIG);
 
         exportGraphPerVocabulary(dataset);
-
-        exportGraphList(dataset);
     }
 
     private void exportGraphPerVocabulary(final Dataset fullDataset) throws FileNotFoundException {
@@ -69,19 +61,5 @@ public class Export {
         map.keySet().forEach(k -> datasetSingle.addNamedModel(k, map.get(k)));
         RDFDataMgr.write(new FileOutputStream("../sgov-graph-per-vocabulary.trig"), datasetSingle,
             RDFFormat.TRIG);
-    }
-
-    private void exportGraphList(final Dataset fullDataset) throws FileNotFoundException {
-        Model model = ModelFactory.createDefaultModel();
-        Resource task = ResourceFactory.createResource(gsp + "Task");
-        Property graph = ResourceFactory.createProperty(gsp + "graph");
-        fullDataset.listNames().forEachRemaining(g -> {
-            final Resource r =
-                ResourceFactory.createResource("http://localhost/" + UUID.randomUUID());
-            model.add(r, RDF.type, task);
-            model.add(r, graph, ResourceFactory.createResource(g));
-        });
-
-        RDFDataMgr.write(new FileOutputStream("../sgov-named-graphs.ttl"), model, RDFFormat.TURTLE);
     }
 }
