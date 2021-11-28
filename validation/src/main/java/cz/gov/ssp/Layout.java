e6e28e7ed7a38965d107879928c54b46898d9f91
Petr Křemen <petr.kremen@gmail.com>
2021-11-28 22:51:45 +0100

    [FIX] Fixing deploy

--- validation/src/main/java/cz/gov/ssp/Layout.java
+++ validation/src/main/java/cz/gov/ssp/Layout.java
@@ -2,18 +2,26 @@ package cz.gov.ssp;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Layout {
 
     private static Set<String> getSubdirectories(String directory) throws IOException {
-        return Files
-            .list(Paths.get(directory))
-            .filter(Files::isDirectory)
-            .map(f -> f.toString())
-            .collect(Collectors.toSet());
+        final Path path = Paths.get(directory);
+        if (Files.exists(path)) {
+            return
+                Files
+                    .list(path)
+                    .filter(Files::isDirectory)
+                    .map(f -> f.toString())
+                    .collect(Collectors.toSet());
+        } else {
+            return Collections.emptySet();
+        }
     }
 
     public static Set<String> getVocabularyFolders() throws IOException {
